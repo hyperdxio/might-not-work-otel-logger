8a664613c2ffd902c1c3a73655c25a99d97a8442
Warren <5959690+wrn14897@users.noreply.github.com>
2024-02-01 14:12:15 -0800

    docs: add comments

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
+// initialize a logger like new Logger({ ..options })
+
 app.get('/', (req: Request, res: Response) => {
+  // fire the logger like logger.info({ foo: 'bar' })
+
   res.send('Hello, Express!');
 });
 
